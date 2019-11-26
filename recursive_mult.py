9f82f53d40ce0716e1029608b7f5c9d961fe5878
DusanJovicic <dusanjovicic197@gmail.com>
2019-11-26 13:33:52 +0100

    Debugged and finished!

--- recursive_mult.py
+++ recursive_mult.py
@@ -1,17 +1,22 @@
 def RecMultInt(x, y):
-
-    x = str(x)
-    y = str(y)
+    """Muliplies two positive integers via D&C recursion"""
 
     # base case
-    if len(x)==1 or len(y)==1 :
+    if len(str(x))==1 or len(str(y))==1 :
         return int(x) * int(y)
     
-    # lengths of x and y
+    # lengths of x and y...
+    x = str(x)
+    y = str(y)
     n = len(x)
     m = len(y)
 
-    # halving x and y into a,b and c,d
+    # ...and their 'ceil' halves used for combining parts
+    nby2 = n//2 + 1 if n%2 == 1 else n//2
+    mby2 = m//2 + 1 if m%2 == 1 else m//2
+
+    # halving x and y into a,b and c,d 
+    #   using 'floor' halves of lengths
     a = x[: n//2]
     b = x[n//2 :]
     c = y[: m//2]
@@ -24,18 +29,10 @@ def RecMultInt(x, y):
     bd = RecMultInt(int(b), int(d))
 
     # combine adding zeros and return
-    product  = ac * 10**(n//2 + m//2)
-    product += ad * 10**(n//2)
-    product += bc * 10**(m//2)
+    product  = ac * 10**(nby2 + mby2)
+    product += ad * 10**(nby2)
+    product += bc * 10**(mby2)
     product += bd
     return product
     
-    
-    #test
-    print(a + ' ' +b)
-    print(c + ' ' +d)
-
-
-
-
-print(RecMultInt(123, 456))
+print(str(RecMultInt(123156, 12987)) + ' tacno je ' + str(123156*12987))
